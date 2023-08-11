aaec67a8a9b39c093ae08157eabacd1be66283e2
윤승재 <sjyun011278@gmail.com>
2023-08-11 12:21:07 +0900

    [FIX] : 🐛: 로그인

--- app/src/main/java/com/spartalearning/selfintoduction/SignInActivity.kt
+++ app/src/main/java/com/spartalearning/selfintoduction/SignInActivity.kt
@@ -1,10 +1,14 @@
 package com.spartalearning.selfintoduction
 
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -13,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity
 class SignInActivity : AppCompatActivity() {
 
     lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var user: User
     private lateinit var loginId: EditText
     private lateinit var loginPassword: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,17 +30,17 @@ class SignInActivity : AppCompatActivity() {
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == RESULT_OK) {
                     val data = result.data
-                    val id = data?.getStringExtra("id")
+                    val resultId = data?.getStringExtra("id")
                     val password = data?.getStringExtra("password")
-                    Toast.makeText(this, "ID: $id, Password: $password", Toast.LENGTH_SHORT).show()
+                    showToast(this, "ID: $resultId, Password: $password")
 
                 }
             }
     }
 
-    //login 버튼을 누르면 loginActivity로 이동
+    //회원 가입 버튼을 누르면 SignUpActivity 이동
     fun signUp(view: View) {
-        //버튼을 클릭하면 SignInActivity로 이동
+        //버튼을 클릭하면 SignUpActivity 이동
         val intent = Intent(this, SignUpActivity::class.java)
         activityResultLauncher.launch(intent)
 
@@ -45,17 +50,41 @@ class SignInActivity : AppCompatActivity() {
         val userId = loginId.text.toString().trim()
         val userPassword = loginPassword.text.toString().trim()
         if (userId.isEmpty() || userPassword.isEmpty()) {
-            Toast.makeText(
-                this, getString(R.string.id_password_msg), Toast.LENGTH_SHORT
-            ).show()
+            showToast(
+                this, getString(R.string.id_password_msg)
+            )
         } else {
-            Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, HomeActivity::class.java)
-            intent.putExtra("id", userId) //id라는 key값을 넘긴다.
-            startActivity(intent)
+            val id = intent.getStringExtra("id").toString()
+            val password = intent.getStringExtra("password").toString()
+            val name = intent.getStringExtra("name").toString()
+            user = User(id, password, name)
+            val array = mutableListOf(user)
+            array.add(user)
+            if (userId == user.id && userPassword == user.password) {
+                showToast(this, "로그인 성공")
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.putExtra("id", userId) //id라는 key값을 넘긴다.
+                startActivity(intent)
+            } else {
+                showToast(this, "로그인 실패")
+            }
         }
     }
 
+    fun showToast(context: Context, message: String) {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val layout: View = inflater.inflate(R.layout.custom_toast, null)
+
+        val text: TextView = layout.findViewById(R.id.customToastText)
+        text.text = message
+
+        with(Toast(context)) {
+            setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL, 0, 100)
+            duration = Toast.LENGTH_SHORT
+            view = layout
+            show()
+        }
+    }
 
 }
 
