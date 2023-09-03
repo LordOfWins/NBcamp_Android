5a30edc2145f0e9f00b28178b022439fc42e251b
윤승재 <sjyun011278@gmail.com>
2023-09-03 18:42:18 +0900

    [RENAME] : ✏️: SNS_Project 폴더 이동

---
+++ Team_Project/SNS_Project/app/src/main/java/com/example/android_supporters_sns_project/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.android_supporters_sns_project
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        
+        splashScreen()
+    }
+    
+    private fun splashScreen() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 500)
+    }
+}
